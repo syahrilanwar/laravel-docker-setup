9632195b13bd53840c671e3a60afeba31951a69b
Syahril Anwar <syahrilanwar.dev@gmail.com>
2024-09-05 16:54:46 +0800

    feat: install and configure Laravel Breeze

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,17 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.jsx',
             refresh: true,
         }),
-        
+        react(),
     ],
-    server: {
-        host: '0.0.0.0', // Akses dari luar container
-        port: 3000, // Port yang sama seperti yang dipetakan dalam docker-compose.yml
-        strictPort: true, // Opsional: Jika true, server akan berhenti jika port tidak tersedia
-    },
 });
